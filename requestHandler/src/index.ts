7c599ee509ef0c168fd8ece51f1d46b193aa8966
Rohan Choudhary <rohan.c2002@gmail.com>
2024-12-14 11:14:53 +0530

    requestHandler init

---
+++ requestHandler/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import { S3 } from "aws-sdk";
+import * as dotenvSafe from 'dotenv-safe';
+
+dotenvSafe.config({
+    path: '.env',
+    example: '.env.example'
+});
+
+const s3 = new S3({
+    accessKeyId: process.env.CLOUDFLARE_ACCESS_KEY_ID,
+    secretAccessKey: process.env.CLOUDFLARE_SECRET_ACCESS_KEY,
+    endpoint: process.env.CLOUDFLARE_ENDPOINT
+})
+
+const app = express();
+
+app.get('/', async(req, res) => {
+    const host = req.hostname;
+
+    const id = host.split(".")[0];
+    const filePath = req.path;
+
+    const contents = await s3.getObject({
+        Bucket: process.env.BUCKET!,
+        Key: `dist/${id}${filePath}`
+    }).promise();
+
+    const type = filePath.endsWith("html") ? "text/html" : filePath.endsWith("css") ? "text/css" : "application/javascript"
+    res.set("Content-Type", type);
+
+    res.send(contents.Body);
+})
+
+app.listen(3001);
